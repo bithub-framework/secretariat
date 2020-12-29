4113b52c035221fd77ea3d73927ee5774801e3ca
Zim <zimtsui@gmail.com>
2020-12-29 16:02:30 +0800

    dev

--- dist/config.js
+++ dist/config.js
@@ -1,6 +1,7 @@
 import { homedir } from 'os';
 import { resolve } from 'path';
 export const DATABASE_PATH = resolve(homedir(), './.bithub/secretariat.db');
+// No trailing slash
 export const REDIRECTOR_URL = 'http://localhost:12000';
 export const LOCAL_HOSTNAME = 'localhost';
 //# sourceMappingURL=config.js.map
